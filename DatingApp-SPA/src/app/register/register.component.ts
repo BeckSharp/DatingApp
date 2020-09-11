0551c3ffeb36d9b14f1cc873e154ea5ab48b118a
Thomas Beck <T.Beck@2019.ljmu.ac.uk>
2020-09-11 19:31:13 +0100

    Added three third party libraries

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
+
+// tslint:disable: typedef
 
 @Component({
   selector: 'app-register',
@@ -10,22 +13,19 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
-  // tslint:disable-next-line: typedef
   ngOnInit() {
   }
 
-  // tslint:disable-next-line: typedef
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('registration successful');
+      this.alertify.success('registration successful');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
-  // tslint:disable-next-line: typedef
   cancel() {
     this.cancelRegister.emit(false);
     console.log('cancelled');
